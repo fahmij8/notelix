e1d2a8aa60723092f72d057407e0927f2b1e4af7
Fahmi Jabbar <rootmawar77@gmail.com>
2022-05-19 23:31:12 +0700

    test: :white_check_mark: Change way to find input element by its placeholder

--- src/components/Input/Input.test.tsx
+++ src/components/Input/Input.test.tsx
@@ -2,8 +2,8 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import Input from './Input';
 
 test('Input should working as expected', () => {
-  render(<Input name="test" label="test" />);
-  const inputElement = screen.getByLabelText('test') as HTMLInputElement;
+  render(<Input name="test" placeholder="test" />);
+  const inputElement = screen.getByPlaceholderText('test') as HTMLInputElement;
   fireEvent.change(inputElement, { target: { value: 'test' } });
   expect(inputElement.value).toBe('test');
 });
