429dc0f3ccda499d0b13ecb900e614aa26805bea
Fahmi Jabbar <rootmawar77@gmail.com>
2022-05-19 22:00:03 +0700

    chore: :art: add design system colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}',
-    './node_modules/tw-elements/dist/js/**/*.js',
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './node_modules/tw-elements/dist/js/**/*.js'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#6F6FC8',
+        secondary: '#FEBE47',
+        success: '#39DB6C',
+        danger: '#F97748',
+        warning: '#FFCC00',
+        info: '#62C1FC',
+      },
+    },
   },
-  plugins: [
-    require('tw-elements/dist/plugin')
-  ]
+  plugins: [require('tw-elements/dist/plugin')],
 };
