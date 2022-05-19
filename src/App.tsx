a8d5a94511b39e8e251d319b977e2ff1f91b74dc
Fahmi Jabbar <rootmawar77@gmail.com>
2022-05-20 04:35:46 +0700

    feat: Implement current available components to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import { useMemo, useState } from 'react';
 import { SearchValueContext } from 'hooks';
 import Navbar from 'components/Navbar';
+import Form from 'components/Form';
+import { getInitialData } from 'utils';
+import { Note } from 'types';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
-  const [value, setValue] = useState('');
-  const searchValue = useMemo(() => ({ value, setValue }), [value, setValue]);
+  const [searchValue, setSearchValue] = useState('');
+  const searchValueMemo = useMemo(
+    () => ({ value: searchValue, setValue: setSearchValue }),
+    [searchValue, setSearchValue],
+  );
+  const [notes, setNotes] = useState<Note[]>(getInitialData());
   return (
-    <SearchValueContext.Provider value={searchValue}>
+    <SearchValueContext.Provider value={searchValueMemo}>
       <div className="App">
         <Navbar />
+        <Form notes={notes} setNotes={setNotes} />
+        <Toaster />
       </div>
     </SearchValueContext.Provider>
   );
