5d2368e154a6d494b661fd60f451e2b96f3396e6
Fahmi Jabbar <rootmawar77@gmail.com>
2022-05-19 22:07:36 +0700

    feat: :hammer: create Logo component

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,123 @@
+function Logo() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      version="1.1"
+      viewBox="270.91 351.6 482.18 64.8"
+      data-testid="app-logo"
+    >
+      <linearGradient x1="30%" x2="-10%" y1="120%" y2="30%" fill="#FFF" spreadMethod="pad">
+        <stop fill="#FFF" offset="0%" stopColor="#fff" stopOpacity="1" />
+        <stop fill="#FFF" offset="100%" stopColor="#000" stopOpacity="1" />
+      </linearGradient>
+      <linearGradient
+        x1="30%"
+        x2="-10%"
+        y1="120%"
+        y2="30%"
+        fill="#FFF"
+        gradientTransform="rotate(-30)"
+        spreadMethod="pad"
+      >
+        <stop fill="#FFF" offset="0%" stopColor="#fff" stopOpacity="1" />
+        <stop fill="#FFF" offset="50%" stopColor="#ccc" stopOpacity="1" />
+        <stop fill="#FFF" offset="100%" stopColor="#000" stopOpacity="1" />
+      </linearGradient>
+      <g fill="#FFF">
+        <image width="480" height="480" x="272" y="144" display="none" />
+        <g>
+          <image x="0" y="0" display="none" />
+          <g
+            style={{
+              lineHeight: '1',
+              fontVariantLigatures: 'none',
+              textAlign: 'center',
+            }}
+            strokeLinejoin="miter"
+            strokeMiterlimit="2"
+            strokeWidth="0"
+            fontFamily="Raleway Medium Alt1"
+            fontSize="72"
+            fontStyle="normal"
+            fontWeight="500"
+            textAnchor="middle"
+          >
+            <path
+              style={{
+                lineHeight: '1',
+                fontVariantLigatures: 'none',
+                textAlign: 'center',
+              }}
+              fill="#F7F7F7"
+              stroke="#F7F7F7"
+              d="M270.908 415.95h8.1v-35.91l-8.1-10.53zm45.9-63.81v49.68l-39.6-49.77h-6.3v4.86l8.1 10.26 39.06 48.78h6.84v-63.81z"
+            />
+            <path
+              style={{
+                lineHeight: '1',
+                fontVariantLigatures: 'none',
+                textAlign: 'center',
+              }}
+              fill="#E5E5E5"
+              stroke="#E5E5E5"
+              d="M406.526 387.51c-.27 1.89-.63 3.78-1.26 5.58-.99 3.06-2.43 5.76-4.32 8.1a23.35 23.35 0 01-6.84 5.76c-2.79 1.44-5.85 2.16-9.18 2.16s-6.3-.63-9-2.07a20.87 20.87 0 01-6.93-5.58c-1.98-2.34-3.42-4.95-4.5-8.01-.63-1.89-1.08-3.87-1.35-5.94h-8.19c.27 2.88.9 5.76 1.98 8.46 1.35 3.96 3.42 7.38 6.12 10.44 2.61 3.06 5.76 5.49 9.45 7.29 3.69 1.8 7.83 2.7 12.33 2.7 4.32 0 8.28-.81 11.97-2.61 3.69-1.8 6.84-4.14 9.54-7.11 2.7-2.97 4.77-6.39 6.3-10.35 1.08-2.79 1.8-5.76 2.16-8.82zm-42.03-12.69c.99-2.97 2.43-5.58 4.32-8.01 1.89-2.34 4.23-4.23 6.93-5.67 2.7-1.44 5.76-2.16 9.18-2.16 3.15 0 6.12.72 8.82 2.07 2.7 1.44 4.95 3.24 6.93 5.58 1.98 2.34 3.42 4.95 4.5 8.01.54 1.62.99 3.33 1.26 4.95h8.19c-.36-2.43-.99-4.95-1.8-7.29-1.44-3.87-3.51-7.29-6.12-10.44-2.7-3.06-5.85-5.49-9.45-7.38-3.69-1.89-7.83-2.88-12.24-2.88s-8.37.9-12.06 2.7c-3.69 1.8-6.84 4.14-9.54 7.11-2.7 3.06-4.86 6.48-6.39 10.35-.99 2.61-1.71 5.22-1.98 7.83h8.19c.27-1.53.72-3.15 1.26-4.77z"
+            />
+            <path
+              style={{
+                lineHeight: '1',
+                fontVariantLigatures: 'none',
+                textAlign: 'center',
+              }}
+              fill="#D3D3D3"
+              stroke="#D3D3D3"
+              d="M458.987 415.95h8.1v-48.6h-8.1zm-22.05-63.9v7.2h52.2v-7.2z"
+            />
+            <path
+              style={{
+                lineHeight: '1',
+                fontVariantLigatures: 'none',
+                textAlign: 'center',
+              }}
+              fill="#C1C1C1"
+              stroke="#C1C1C1"
+              d="M525.102 359.25h34.47v-7.2h-42.57v19.8h8.1zm-8.1 49.5v7.2h43.38v-7.2zm8.1-22.05h29.97v-6.75h-38.07v20.7h8.1z"
+            />
+            <path
+              style={{
+                lineHeight: '1',
+                fontVariantLigatures: 'none',
+                textAlign: 'center',
+              }}
+              fill="#9D9D9D"
+              stroke="#9D9D9D"
+              d="M590.244 408.75v7.2h43.74v-7.2zm8.1-56.7h-8.1v48.6h8.1z"
+            />
+            <path
+              style={{
+                lineHeight: '1',
+                fontVariantLigatures: 'none',
+                textAlign: 'center',
+              }}
+              fill="#8D8D8D"
+              stroke="#8D8D8D"
+              d="M661.553 415.95h8.1v-63.9h-8.1z"
+            />
+            <path
+              style={{
+                lineHeight: '1',
+                fontVariantLigatures: 'none',
+                textAlign: 'center',
+              }}
+              fill="#929191"
+              stroke="#929191"
+              d="M720.422 372.84l-15.12-20.79h-9l19.71 26.64zm32.67-20.79h-8.91l-19.44 26.64-4.41 5.76-23.31 31.5h9l18.72-25.74 18.72 25.74h8.91l-23.31-31.5z"
+            />
+          </g>
+          <image x="0" y="0" display="none" />
+        </g>
+      </g>
+    </svg>
+  );
+}
+
+export default Logo;
