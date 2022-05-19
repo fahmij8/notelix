f1c24c37a7189653756fbf74eef1691ae6327cb0
Fahmi Jabbar <rootmawar77@gmail.com>
2022-05-20 01:27:20 +0700

    feat: :zap: Finish Navbar Component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,17 +1,31 @@
+import { useId } from 'react';
 import Logo from 'components/Logo';
-import Input from 'components/Input';
+import Button from 'components/Button';
+import Modal from 'components/Modal';
+import Search from 'components/Search';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 function Navbar() {
+  const MODAL_ID = useId().replace(/:/g, '');
   return (
     <header>
       <nav className="flex flex-row justify-between p-5 items-center">
         <div className="w-28 md:w-36">
           <Logo />
         </div>
-        <div>
-          <Input size="medium" type="text" name="search" placeholder="Search Notes..." />
+        <div className="hidden md:block md:w-1/3">
+          <Search />
+        </div>
+        <div className="md:hidden">
+          <Button theme="transparent" isModalTrigger modalId={MODAL_ID}>
+            <FontAwesomeIcon icon={faSearch} size="2x" />
+          </Button>
         </div>
       </nav>
+      <Modal id={MODAL_ID}>
+        <Search className="mt-16" />
+      </Modal>
     </header>
   );
 }
