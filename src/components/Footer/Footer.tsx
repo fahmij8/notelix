1a32cd0ffa9bd49a5136647c21fea26617fbe45b
Fahmi Jabbar <rootmawar77@gmail.com>
2022-05-20 07:14:01 +0700

    feat: :hammer: Create Footer Component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+function Footer(): JSX.Element {
+  return (
+    <footer className="text-center lg:text-left">
+      <div className="text-slate-300 text-center p-2 text-xs">
+        ©2022 - Made with
+        <span className="text-red-500"> ❤ </span>
+        by Fahmi Jabbar
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
